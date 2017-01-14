ec2767cfe49cd278bdf3352bc6988bd646cd11f3
Philina1nisia <Philina1nisia@gmail.com>
2017-01-14 17:49:01 +0800

    star

--- js/final.js
+++ js/final.js
@@ -1,61 +1,108 @@
-window.addEventListener('scroll', function(event) {
-  var depth, i, layer, layers, len, movement, topDistance, translate3d;
-  topDistance = this.pageYOffset;
-  layers = document.querySelectorAll("[data-type='parallax']");
-  for (i = 0, len = layers.length; i < len; i++) {
-    layer = layers[i];
-    depth = layer.getAttribute('data-depth');
-    movement = -(topDistance * depth);
-    translate3d = 'translate3d(0, ' + movement + 'px, 0)';
-    layer.style['-webkit-transform'] = translate3d;
-    layer.style['-moz-transform'] = translate3d;
-    layer.style['-ms-transform'] = translate3d;
-    layer.style['-o-transform'] = translate3d;
-    layer.style.transform = translate3d;
-  }
+window.addEventListener('scroll', function (event) {
+    var depth, i, layer, layers, len, movement, topDistance, translate3d;
+    topDistance = this.pageYOffset;
+    layers = document.querySelectorAll("[data-type='parallax']");
+    for (i = 0, len = layers.length; i < len; i++) {
+        layer = layers[i];
+        depth = layer.getAttribute('data-depth');
+        movement = -(topDistance * depth);
+        translate3d = 'translate3d(0, ' + movement + 'px, 0)';
+        layer.style['-webkit-transform'] = translate3d;
+        layer.style['-moz-transform'] = translate3d;
+        layer.style['-ms-transform'] = translate3d;
+        layer.style['-o-transform'] = translate3d;
+        layer.style.transform = translate3d;
+    }
 });
-
-(function($) { 
-    
-    $(document).ready(function(){    
-        var actualBottom = $(about2).offset().top + $(about2).outerHeight(true);
-    
-        $(window).scroll(function(){                          
-            if ($(this).scrollTop() > actualBottom) {
-                $('#menusmall').fadeIn(500);
-            } else {
-                $('#menusmall').fadeOut(500);
-            }
-        });
+$(document).ready(function () {
+    var actualBottom = $(about2).offset().top + $(about2).outerHeight(true);
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > actualBottom) {
+            $('#menusmall').fadeIn(200);
+        }
+        else {
+            $('#menusmall').fadeOut(200);
+        }
     });
-})(jQuery);
-//
-//  $(document).ready(function(){
-//
-//    // hide .navbar first
-//    $("#menusmall").hide();
-//
-//    // fade in .navbar
-//    $(function () {
-//        $(window).scroll(function () {
-//
-//                 // set distance user needs to scroll before we start fadeIn
-//            if ($(this).scrollTop() > 100) {
-//                $('#menusmall').fadeIn();
-//            } else {
-//                $('#menusmall').fadeOut();
-//            }
-//        });
-//    });
-//
-//});
+});
+$(document).ready(function () {
+    var rotation = 0;
+    $(star2).click(function () {
+        $('#star_menu').css({
+            'transform': 'rotate(' + 10 + 'deg)'
+        })
+        $('#type1').addClass("animated fadeInRight slide")
+        $('#type_head').css("visibility", "hidden")
+        $('#type3').removeClass("animated fadeInRight slide");
+        $('#type4').removeClass("animated fadeInRight slide");
+        $('#type5').removeClass("animated fadeInRight slide");
+        $('#type2').removeClass("animated fadeInRight slide");
+//        
+//        $('#type_head').css("visibility", "hidden");
+//        $('#type2').css("visibility", "visible");
+//        $('#type1').css("visibility", "hidden");
+//        $('#type3').css("visibility", "hidden");
+//        $('#type4').css("visibility", "hidden");
+//        $('#type5').css("visibility", "hidden");
+    });
+   
+       $(star_center).click(function () {
+        $('#star_menu').css({
+            'transform': 'rotate(' + 0 + 'deg)'
+        })
+        $('#type_head').css("visibility", "visible")
+        $('#type3').removeClass("animated fadeInRight slide")
+        $('#type4').removeClass("animated fadeInRight slide")
+        $('#type5').removeClass("animated fadeInRight slide")
+        $('#type1').removeClass("animated fadeInRight slide")
+        $('#type2').removeClass("animated fadeInRight slide")
 
+    }); 
+    
+    
+    $(star3).click(function () {
+        $('#star_menu').css({
+            'transform': 'rotate(' + 300 + 'deg)'
+        })
+        $('#type2').addClass("animated fadeInRight slide")
+        $('#type_head').css("visibility", "hidden")
+        $('#type3').removeClass("animated fadeInRight slide");
+        $('#type4').removeClass("animated fadeInRight slide");
+        $('#type5').removeClass("animated fadeInRight slide");
+        $('#type1').removeClass("animated fadeInRight slide");
 
-
-//
-//$(function(){
-//  $('#hero, .layer').css({ height: $(window).innerHeight() });
-//  $(window).resize(function(){
-//    $('#hero, .layer').css({ height: $(window).innerHeight() });
-//  });
-//});
+    });
+    $(star4).click(function () {
+        $('#star_menu').css({
+            'transform': 'rotate(' + 230 + 'deg)'
+        })
+          $('#type3').addClass("animated fadeInRight slide")
+        $('#type_head').css("visibility", "hidden")
+        $('#type1').removeClass("animated fadeInRight slide");
+        $('#type4').removeClass("animated fadeInRight slide");
+        $('#type5').removeClass("animated fadeInRight slide");
+        $('#type2').removeClass("animated fadeInRight slide");
+    });
+    $(star5).click(function () {
+        $('#star_menu').css({
+            'transform': 'rotate(' + 150 + 'deg)'
+        })
+          $('#type4').addClass("animated fadeInRight slide")
+        $('#type_head').css("visibility", "hidden")
+        $('#type3').removeClass("animated fadeInRight slide");
+        $('#type1').removeClass("animated fadeInRight slide");
+        $('#type5').removeClass("animated fadeInRight slide");
+        $('#type2').removeClass("animated fadeInRight slide");
+    });
+    $(star1).click(function () {
+        $('#star_menu').css({
+            'transform': 'rotate(' + 70 + 'deg)'
+        })
+          $('#type5').addClass("animated fadeInRight slide")
+        $('#type_head').css("visibility", "hidden")
+        $('#type3').removeClass("animated fadeInRight slide");
+        $('#type4').removeClass("animated fadeInRight slide");
+        $('#type1').removeClass("animated fadeInRight slide");
+        $('#type2').removeClass("animated fadeInRight slide");
+    });
+});
